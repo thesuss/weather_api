39b67d1c6ebf4ca50e499a58295eff1af3dc8e38
thesuss <lullisuss@gmail.com>
2016-08-19 17:18:19 +0200

    it works on the webpage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
 var onReadyEvents = function(){
+  var myDelay = 6000;
+
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   };
 
-
   function success(pos) {
     crd = pos.coords;
-    console.log('Latitude : ' + crd.latitude);
-    console.log('Longitude: ' + crd.longitude);
     lat = crd.latitude.toFixed(2);
     long = crd.longitude.toFixed(2);
     url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=072af5559931a9dc9c97e0a217a48f07&units=metric'
-
   };
 
   function error(err) {
@@ -21,27 +19,29 @@ var onReadyEvents = function(){
   };
 
   navigator.geolocation.getCurrentPosition(success, error, options);
+  setTimeout(callAjax, myDelay);
 };
 
-// function weatherString(url) {
-//   //  weatherList = getJSON('url')
-//     $.ajax({
-//       dataType: "jsonp",
-//       url: url,
-//       type: 'GET',
-//       cache: false,
-//       success: function (data) {
-//         place = data.name
-//         $('#place').html(place);
-//         info = data.weather[0].main;
-//         $('#info').html(info);
-//         description = data.weather[0].description;
-//         $('#description').html(description);
-//         temperature = data.main.temp.toFixed(1);
-//         $('#temperature').html(temperature);
-//       },
-//       error: function (dataError) {
-//         alert("No weather data");
-//       }
-//     };
-//   };
+// function that processes your ajax calls...
+function callAjax() {
+    // check to see if there are id's remaining...
+    $.ajax({
+           dataType: "jsonp",
+           url: url,
+           type: 'GET',
+           cache: false,
+          success: function (data) {
+           place = data.name
+           $('#place').html(place);
+           info = data.weather[0].main;
+           $('#info').html(info);
+           description = data.weather[0].description;
+           $('#description').html(description);
+           temperature = data.main.temp.toFixed(1);
+           $('#temperature').html(temperature);
+         },
+         error: function (dataError) {
+           alert("No weather data");
+         }
+    });
+  };
